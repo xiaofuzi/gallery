5aba3caeaf64d2f6b265226f9ef989ff1a0dbee2
小福 <mynameisfushengyang@gmail.com>
2015-11-26 11:37:58 +0800

    轮播实现，v0.1.1

---
+++ js/slider.js
@@ -0,0 +1,112 @@
+var Silder = {
+	silderIndexs: '.silder-index',
+	leftBtn: '#leftBtn',
+	rightBtn: '#rightBtn',
+	itemWrap: '#itemWrap',
+	subItem: '#itemWrap .sub-item',
+	init: function(params){
+		if(params){
+			this.sliderCreate(params);
+		}
+
+		this.scroll();
+		this.autoTurnLeft();
+		this.sliderIndex();
+	},
+	//初始配置
+	sliderCreate(params){
+		for(var ele in params){
+			if(params[ele]){
+				Silder[ele] = params[ele];
+			}
+		}
+	},
+	scroll: function(){
+		//jquery对象实例化
+		var $itemWrap = $(this.itemWrap);
+		var $imgItems = $(this.subItem);
+		var $leftBtn = $(this.leftBtn);
+		var $rightBtn = $(this.rightBtn);
+		var $silderIndexs = $(this.silderIndexs);
+		
+		$leftBtn.on('click', function(){
+			$itemWrap.animate({
+				'margin-left': 0 - $imgItems.width()
+			}, 1000, function(){
+				$itemWrap.append($(Silder.subItem).first());
+				$itemWrap.css("margin-left",0);
+			})
+			//对应导航图标激活状态绑定
+			var currentIndex = $(Silder.subItem).first().attr('data-index');
+			$silderIndexs.each(function(){
+				$this = $(this);
+				if($this.attr('data-index') == (currentIndex-0+1)){
+					$this.addClass('active');
+				}else{
+					$this.removeClass('active');
+				}
+			})
+		})
+		$rightBtn.on('click', function(){
+			$itemWrap.prepend($('#itemWrap .sub-item').last());
+				$itemWrap.css("margin-left",0 - $imgItems.width());
+			$itemWrap.animate({
+				'margin-left': 0
+			}, 1000)
+
+			//对应导航图标激活状态绑定
+			var currentIndex = $(Silder.subItem).last().attr('data-index');
+			$silderIndexs.each(function(){
+				$this = $(this);
+				if($this.attr('data-index') == (currentIndex-0+1)){
+					$this.addClass('active');
+				}else{
+					$this.removeClass('active');
+				}
+			})
+		})
+	},
+	//自动轮播
+	autoTurnLeft: function(){
+		var $leftBtn = $(this.leftBtn);
+		setInterval(function(){
+			$leftBtn.trigger('click');
+		}, 5000)
+	},
+	sliderIndex: function(){
+		//jquery对象实例化
+		var $itemWrap = $(this.itemWrap);
+		var $imgItems = $(this.subItem);
+		var $silderIndexs = $(this.silderIndexs);
+
+		$silderIndexs.each(function(){
+			$this = $(this);
+			$this.on('click', function(){
+				//jquery对象实例化
+				$itemWrap = $(Silder.itemWrap);
+				$imgItems = $(Silder.subItem);
+				$silderIndexs = $(Silder.silderIndexs);
+
+				$$this = $(this);
+				var index = $$this.attr('data-index');
+				$imgItems.each(function(){
+					_this = $(this);
+					if(_this.attr('data-index') == index){
+						$itemWrap.css('margin-left', 0);
+						//退出循环
+						return false;	
+					}else{
+						$itemWrap.append($(Silder.subItem).first());
+					}
+					//导航按钮状态切换
+					$silderIndexs.removeClass('active');
+					$$this.addClass('active');				
+				})
+			})
+		})
+	}
+}
+
+$(function(){
+	Silder.init();
+})
---
+++ readme.md
@@ -0,0 +1,59 @@
+## 简介
+
+轮播效果实现，依赖于jQuery,默认样式为全屏轮播，如有需要可自定义样式。
+
+## 如何使用
+
+1. HTML结构
+
+```html
+<div class="page-wrapp">
+	<div class="item-wrap">
+		<div class="sub-item"></div>
+		<div class="sub-item"></div>
+		<div class="sub-item"></div>
+		<div class="sub-item"></div>
+		<div class="sub-item"></div>
+	</div>
+</div>
+```
+
+默认结构为三次，轮播元素为5个，轮播图可放在.sub-item之下，即
+
+```html
+<div class="subitem">
+	<img src="/my-image.jpg" alt="">
+</div>
+```
+这样就添加了一张图片元素
+
+
+2. 自定义样式，在main.css之后引入
+
+* 给page-wrapp设宽高
+
+```css
+.page-wrapp{
+	/*调整位置可在此设置*/
+	position: relative;
+	/*轮播外层框架宽高设置*/
+	width: 500px;
+	height: 400px;
+}
+
+.page-wrapp .item-wrap{
+	/*宽度默认为500%(若有4个子元素，则改为400%)
+	高度默认为100%*/
+	width: 500%;
+}
+```
+
+3. 脚本引入
+
+按顺序引入jquery、slider.js,路径可自定义
+
+```javascript
+<script src="./js/jquery-2.1.4"></script>
+<script src="./js/slider.js"></script>
+```
+
